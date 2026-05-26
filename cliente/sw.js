5b3b2f06953657315650b49b15ac37e31d6ba62f
eduvc71-code <eduvc71-code@users.noreply.github.com>
2026-05-25 21:38:14 -0400

    Updates

--- cliente/sw.js
+++ cliente/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rapidingo-cliente-mplwgitq';
+const CACHE_NAME = 'rapidingo-cliente-mplyuh81';
 const urlsToCache = [
   './',
   './index.html',
--- delivery/sw.js
+++ delivery/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rapidingo-delivery-mplwgitq';
+const CACHE_NAME = 'rapidingo-delivery-mplyuh81';
 const urlsToCache = [
   './',
   './index.html',
